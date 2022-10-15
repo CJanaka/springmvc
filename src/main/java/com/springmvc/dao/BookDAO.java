4374c4324cd2e1ff168c7d817809b94c164a9ca3
Janaka Chathuranga <nv99chathuranga@gmail.com>
2022-10-15 13:13:08 +0530

    Initial commit

---
+++ src/main/java/com/springmvc/dao/BookDAO.java
@@ -0,0 +1,14 @@
+package com.springmvc.dao;
+
+import java.util.List;
+
+import com.springmvc.model.Book;
+
+public interface BookDAO {
+
+	   long save(Book book);
+	   Book get(long id);
+	   List<Book> list();
+	   void update(long id, Book book);
+	   void delete(long id);
+}
